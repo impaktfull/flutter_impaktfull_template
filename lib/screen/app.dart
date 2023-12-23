7a58ff6c3cff323bb5cb433a55873f800258c3f3
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2023-12-23 14:03:15 +0100

    fix: Cleanup the barebone example

---
+++ lib/screen/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/screen/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Template',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(
+      ),
+    );
+  }
+}
